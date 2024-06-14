59ead36cd8c54d43012f2320041c78e1fac2cdd1
rohitmenonhart-xhunter <rohitmenongeneral@gmail.com>
2024-06-14 11:12:10 +0530

    first commit

--- app.js
+++ app.js
@@ -61,10 +61,40 @@ function displayDashboard() {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // Add a marker at the center
-    // L.marker([13.0827, 80.2707]).addTo(map)
-    //     .bindPopup('SVCE Campus')
-    //     .openPopup();
+    // Custom icons for markers
+    const icon1 = L.icon({
+        iconUrl: 'icon1.png', // Replace 'icon1.png' with your custom icon URL
+        iconSize: [32, 32], // Size of the icon
+        iconAnchor: [16, 32], // Point of the icon which will correspond to marker's location
+        popupAnchor: [0, -32] // Point from which the popup should open relative to the iconAnchor
+    });
+
+    const icon2 = L.icon({
+        iconUrl: 'icon2.png', // Replace 'icon2.png' with your custom icon URL
+        iconSize: [32, 32],
+        iconAnchor: [16, 32],
+        popupAnchor: [0, -32]
+    });
+
+    const icon3 = L.icon({
+        iconUrl: 'icon3.png', // Replace 'icon3.png' with your custom icon URL
+        iconSize: [32, 32],
+        iconAnchor: [16, 32],
+        popupAnchor: [0, -32]
+    });
+
+    // Add markers with custom icons
+    L.marker([13.0827, 80.2707], { icon: icon1 }).addTo(map)
+        .bindPopup('SVCE Campus')
+        .openPopup();
+
+    L.marker([13.07, 80.27], { icon: icon2 }).addTo(map)
+        .bindPopup('Location 2')
+        .openPopup();
+
+    L.marker([13.09, 80.26], { icon: icon3 }).addTo(map)
+        .bindPopup('Location 3')
+        .openPopup();
 }
 
 function saveUserInfoToFirebase(registerNumber) {
